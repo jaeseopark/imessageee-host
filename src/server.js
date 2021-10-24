616e3e2427cc07c8dc37e0060070160eed7fc49e
Jaeseo Park <20038316+jaeseopark@users.noreply.github.com>
2021-10-23 23:07:57 -0600

    add IMFMessage

--- src/server.js
+++ src/server.js
@@ -11,56 +11,53 @@ enableWs(app);
 
 app.use(express.json());
 app.use(
-  cors({
-    origin: "*",
-  })
+    cors({
+        origin: "*",
+    })
 );
 
 const SUCCESS_JSON = {
-  result: "success",
+    result: "success",
 };
 
-const sendMessagee = (message, recipient, attachment = null) =>
-  getTextScript(message, recipient).then(runAppleScript);
+const sendMessagee = (message, recipient, attachment = null) => getTextScript(message, recipient).then(runAppleScript);
 
 app.get("/contacts", (_, res) => {
-  // TODO: pull live data from the Contacts app.
-  res.send({
-    Khloe: "+17801234567",
-    Jaeseo: "+17807654321",
-  });
+    // TODO: pull live data from the Contacts app.
+    res.send({
+        Khloe: "+17801234567",
+        Jaeseo: "+17807654321",
+    });
 });
 
 app.post("/msg", (req, res) => {
-  console.log(req.body);
-  sendMessagee(req.body.message, req.body.recipient, req.body.attachment).then(
-    () => res.send(SUCCESS_JSON)
-  );
+    console.log(req.body);
+    sendMessagee(req.body.message, req.body.recipient, req.body.attachment).then(() => res.send(SUCCESS_JSON));
 });
 
 app.ws("/msg", (ws) => {
-  ws.on("message", (msg) => {
-    if (!isValidJson(msg)) {
-      return ws.send(
-        JSON.stringify({
-          error: "Not a valid json string",
-        })
-      );
-    }
+    ws.on("message", (msg) => {
+        if (!isValidJson(msg)) {
+            return ws.send(
+                JSON.stringify({
+                    error: "Not a valid json string",
+                })
+            );
+        }
 
-    const reqBody = JSON.parse(msg);
-    console.log(reqBody);
+        const reqBody = JSON.parse(msg);
+        console.log(reqBody);
 
-    sendMessagee(reqBody.message, reqBody.recipient, reqBody.attachment)
-      .then(() => ws.send(JSON.stringify(SUCCESS_JSON)))
-      .catch((error) =>
-        ws.send(
-          JSON.stringify({
-            error,
-          })
-        )
-      );
-  });
+        sendMessagee(reqBody.message, reqBody.recipient, reqBody.attachment)
+            .then(() => ws.send(JSON.stringify(SUCCESS_JSON)))
+            .catch((error) =>
+                ws.send(
+                    JSON.stringify({
+                        error,
+                    })
+                )
+            );
+    });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
