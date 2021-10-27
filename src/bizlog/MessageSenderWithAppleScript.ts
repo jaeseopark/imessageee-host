3e0ded57d8cab588a538cc89974e8c5643f550ec
jaeseopark <20038316+jaeseopark@users.noreply.github.com>
2021-10-26 19:49:54 -0600

    embed apple script in MessageSender

--- src/bizlog/MessageSenderWithAppleScript.ts
+++ src/bizlog/MessageSenderWithAppleScript.ts
@@ -4,10 +4,20 @@ import {
 
 import IMFMessage from "../datatype/IMFMessage";
 import { MessageSender } from "../interface/MessageHandler";
-import { getTextScript } from "../template";
+
+export const getTextScript = (message: string, recipient: string, service: string) =>
+    Promise.resolve(`
+    tell application "Messages"
+        set targetService to "${service}"
+        set msg to "${message}"
+        set recipient to buddy "${recipient}" of targetService
+        send msg to recipient
+    end tell
+    `);
+
 
 class MessageSenderWithAppleScript implements MessageSender {
-    send = (m: IMFMessage) => getTextScript(m.content.text, m.conversation.handle)
+    send = (m: IMFMessage) => getTextScript(m.content.text, m.handle)
         .then((script) => { runAppleScriptSync(script); });
 
     close = () => {
