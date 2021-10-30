6a46946b3b197338fc244485780166c499177442
Jaeseo Park <20038316+jaeseopark@users.noreply.github.com>
2021-10-30 14:50:14 -0600

    feat: introduce event type

--- src/datatype/IMFEvent.ts
+++ src/datatype/IMFEvent.ts
@@ -1,7 +1,10 @@
 import IMFMessage from "./IMFMessage";
 
+export type IMFEventType = "MESSAGE_PRELOAD" | "MESSAGE_NEW" | "CONTACTS";
+
 type IMFEvent = {
     messages?: IMFMessage[];
+    type: IMFEventType;
 };
 
 export default IMFEvent;
